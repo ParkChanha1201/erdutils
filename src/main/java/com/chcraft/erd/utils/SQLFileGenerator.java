cf62cac9af9fdc4d4cb8e417d0b8ff7f40fed769
박찬하 <chanhacraft@naver.com>
2021-06-22 14:20:48 +0900

    checkout new branch

---
+++ src/main/java/com/chcraft/erd/utils/SQLFileGenerator.java
@@ -0,0 +1,15 @@
+package com.chcraft.erd.utils;
+
+import java.util.List;
+
+import com.chcraft.erd.Entity;
+import com.chcraft.erd.Relationship;
+
+public class SQLFileGenerator implements ERDFileGenerator {
+
+	public String generateFile(List<Entity> entities, List<Relationship> relationships) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
